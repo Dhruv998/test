bf9bf9144419b1211fae22728be287bb01f71232
Dhruv998 <80926796+Dhruv998@users.noreply.github.com>
2024-04-09 17:59:31 -0500

    Add files via upload

---
+++ GOOFYAHHTHINGY.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+public class GOOFYAHHTHINGY {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String first;
+        String last;
+        int age;
+       
+       System.out.println("Enter First Name >>>");
+       first = scanner.nextLine();
+
+       System.out.println("Enter Last Name >>>");
+       last = scanner.nextLine();
+
+       System.out.println("Enter Age >>>");
+       age = scanner.nextInt();
+
+
+       
+       
+        System.out.println("Hello " + first + " " + last + ", age " + age + " " );      
+
+    
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ hello.java
@@ -0,0 +1,9 @@
+public class hello {
+    public static void main(String[] args) {
+        int var = 999999;
+        System.out.println(++var);
+       
+
+        
+    }
+}
